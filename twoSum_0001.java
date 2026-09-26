e6e7a228834e6bdcfd1d7a368e526daa6f68b958
XU-123-456-max <2942442819@qq.com>
2026-09-26 16:01:18 +0800

    add: 001 两数之和

---
+++ twoSum_0001.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+import java.util.Map;
+import java.util.HashMap;
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> table = new HashMap<Integer, Integer>();
+        for(int i = 0;i < nums.length;i++){
+            if(table.containsKey(target - nums[i])){
+                return new int[]{table.get(target - nums[i]), i};
+            }
+            table.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+// @lc code=end
+
